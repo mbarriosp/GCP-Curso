783a2ce89935d50f64b0282aed81f886a1404e3c
mbarriosp <mbarriosp@homecenter.co>
2022-08-10 01:54:11 +0000

    Carga Dtos GitHub

---
+++ autom.sh
@@ -0,0 +1,30 @@
+echo "Hola Clase AulaMatriz, hola" $USER !!!!!
+ 
+echo
+ 
+echo "Veo a estos usuarios conectados al server"
+ 
+echo
+ 
+whois
+ 
+w
+ 
+echo
+ 
+echo "Ahora Ejecutamos el Push a GitHub"
+ 
+
+ ls
+ git commit -m 'Carga Dtos GitHub'
+git  add *.*
+git push 
+
+ 
+echo
+ 
+echo "Espera un Momento......"
+ 
+ls -laxo  > reporte.txt
+ 
+echo " Verifica tu repo en git para confirmar la actualizacion..."
